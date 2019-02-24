6d58640fa90a94ba9b1ca3f7b43ed0204d423efe
Brian <brians931@gmail.com>
2019-02-24 19:40:34 +0300

    create the view object and tested it

---
+++ js/battleship.js
@@ -0,0 +1,29 @@
+
+
+// implementint the view object that will control the displayed messages
+//onn the screen
+
+var view={
+
+  displayMessage:function (msg) { 
+    var messageArea=document.getElementById("messageArea");
+    messageArea.innerHTML=msg;
+
+   },
+
+  displayHit:function (location) { 
+    var cell=document.getElementById(location);
+    cell.setAttribute("class","hit");
+
+   },
+
+   displayMiss:function (location) { 
+    var cell=document.getElementById(location);
+    cell.setAttribute("class","miss");
+
+  }
+};
+
+view.displayHit("00");
+view.displayMessage("testing testing");
+view.displayMiss("54");
